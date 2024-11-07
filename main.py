2d84a7502a65278cee7050d62bb941989244bb6e
Subhasmita <subhasmitaswain232@gmail.com>
2024-11-07 20:22:17 +0530

    replace shell commands to be alpine specific

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def build_and_publish_image(client, os_version, cuda_version, container_ty
         .with_user("root")
         .with_workdir("/app")
         # Install Micromamba
-        .with_exec(["/bin/sh", "-c", "apk add --no-cache curl && curl -Ls https://micro.mamba.pm/install.sh | bash"])
+        .with_exec(["/bin/sh", "-c", "apk add --no-cache curl && curl -Ls https://micro.mamba.pm/install.sh | /bin/sh"])
         # Install packages using Micromamba
         .with_exec(["/bin/sh", "-c", f"micromamba install -y -n base -c conda-forge {container_type} python={python_version} && micromamba clean --all --yes && micromamba list"])
         .with_label("org.opencontainers.image.source", f"https://github.com/{username}/{repository}")
